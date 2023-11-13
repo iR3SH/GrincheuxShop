18d88295d146f5d5a3bc9c8e2e7e6c2c8a0d593f
iR3SH <q7x@hotmail.fr>
2023-11-13 16:45:26 +0100

    hello IDE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./grincheux-shop/grincheux-shop.module').then(m => m.GrincheuxShopModule)
+  },
+  { path : '**',
+  redirectTo: 'grincheuxshop'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AuthInterceptorService} from "./grincheux-shop/services/auth-interceptor.service";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgToastModule} from "ng-angular-popup";
 
+const routes: Routes = [{
+    path: '',
+    loadChildren: () => import('./grincheux-shop/grincheux-shop.module').then(m => m.GrincheuxShopModule)
+  }
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forChild(routes),
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    NgbModule,
+    NgToastModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent
+   ]
 })
 export class AppModule { }
